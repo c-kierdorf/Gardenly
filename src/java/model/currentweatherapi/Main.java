81a0382d4858ac478cde54088741dd5c7a395346
crillos <kierdorf.christian@gmail.com>
2022-04-12 12:48:41 +0200

    Build Main.java

---
+++ src/java/model/currentweatherapi/Main.java
@@ -0,0 +1,69 @@
+package model.currentweatherapi;
+
+public class Main {
+    private Double temp;
+    private Double feelsLike;
+    private Double tempMin;
+    private Double tempMax;
+    private int pressure;
+    private int humidity;
+
+    public Main(Double temp, Double feelsLike, Double tempMin, Double tempMax, int pressure, int humidity) {
+        this.temp = temp;
+        this.feelsLike = feelsLike;
+        this.tempMin = tempMin;
+        this.tempMax = tempMax;
+        this.pressure = pressure;
+        this.humidity = humidity;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(Double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public Double getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(Double tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public Double getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(Double tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+    
+    
+}
