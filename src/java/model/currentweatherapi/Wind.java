8c2a346f6b7090dc112427f06fde8f0084ec5404
crillos <kierdorf.christian@gmail.com>
2022-04-12 12:49:46 +0200

    Build Wind.java

---
+++ src/java/model/currentweatherapi/Wind.java
@@ -0,0 +1,28 @@
+package model.currentweatherapi;
+
+public class Wind {
+    
+    private Double speed;
+    private int deg;
+
+    public Wind(Double speed, int deg) {
+        this.speed = speed;
+        this.deg = deg;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public int getDeg() {
+        return deg;
+    }
+
+    public void setDeg(int deg) {
+        this.deg = deg;
+    }
+}
