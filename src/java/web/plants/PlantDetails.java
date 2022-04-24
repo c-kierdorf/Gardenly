3c838e1e545a01ebbe11cbb457bf35e52862c156
crillos <kierdorf.christian@gmail.com>
2022-04-24 18:10:50 +0200

    Build servlet PlantDetails.java

---
+++ src/java/web/plants/PlantDetails.java
@@ -0,0 +1,102 @@
+package web.plants;
+
+import db.Plant;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.plant.PlantManager;
+import model.user.UserManager;
+
+/**
+ *
+ * @author crill
+ */
+@WebServlet(name = "PlantDetails", urlPatterns = {"/plants/PlantDetails"})
+public class PlantDetails extends HttpServlet {
+    
+    @Inject
+    private PlantManager pm;
+    @Inject
+    private UserManager um;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            if (request.getParameter("p_id") != null) {
+                Integer id = Integer.parseInt(request.getParameter("p_id"));
+                Plant plant = pm.findPlantById(id);
+                if (plant == null) {
+                    pm.setErrors(true);
+                    pm.setStatus("Pflanze mit ID '" + id + "' nicht gefunden.");
+                } else {
+                    pm.setErrors(false);
+                    pm.setPlant(plant);
+                }
+            } else {
+                pm.setErrors(true);
+                pm.setStatus("Es konnte keine Pflanze aufgerufen werden, weil keine Pflanzen ID übergeben wurde.");
+            }
+            
+            RequestDispatcher rd
+                    = request.getRequestDispatcher("PlantDetails.jsp");
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
