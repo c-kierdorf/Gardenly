644cae15dabe016d6904d15f18b3afcbf7f32a3f
crillos <kierdorf.christian@gmail.com>
2022-04-26 21:45:51 +0200

    Moved file subfolder arduino

--- src/java/web/watering/Watering.java
+++ src/java/web/watering/Watering.java
@@ -41,100 +41,10 @@ public class Watering extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
 
-            upm.setErrors(false);
-            Integer id = Integer.parseInt(request.getParameter("id"));
-            UserPlant userPlant = upm.findUserPlantById(id);
+            wateringProcess(request, response);
 
-            if (userPlant != null) {
-
-                // watering
-                String watering = request.getParameter("watering");
-                if (watering != null) {
-                    if (watering.equals("now")) {
-                        Date date = new Date();
-                        userPlant.setWateringDate(date);
-                    }
-                }
-
-                // waterlevel
-                String waterlevel = request.getParameter("waterlevel");
-                if (waterlevel != null) {
-                    try {
-                        Integer waterlevelInt = Integer.parseInt(request.getParameter("waterlevel"));
-                        userPlant.setWaterlevel(waterlevelInt);
-                    } catch (NumberFormatException nfe) {
-                        upm.setErrors(true);
-                        upm.setStatus("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
-                        System.out.println("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
-                        nfe.printStackTrace();
-                    }
-
-                }
-
-                // humidity
-                String humidity = request.getParameter("humidity");
-                if (humidity != null) {
-                    try {
-                        Integer humidityInt = Integer.parseInt(request.getParameter("humidity"));
-                        userPlant.setHumidityNow(humidityInt);
-                    } catch (NumberFormatException nfe) {
-                        upm.setErrors(true);
-                        upm.setStatus("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
-                        System.out.println("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
-                        nfe.printStackTrace();
-                    }
-                }
-
-                // light
-                String light = request.getParameter("light");
-                if (light != null) {
-                    try {
-                        Integer lightInt = Integer.parseInt(request.getParameter("light"));
-                        userPlant.setLightNow(lightInt);
-                    } catch (NumberFormatException nfe) {
-                        upm.setErrors(true);
-                        upm.setStatus("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
-                        System.out.println("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
-                        nfe.printStackTrace();
-                    }
-                }
-
-                // temperature
-                String temperature = request.getParameter("temperature");
-                if (temperature != null) {
-                    try {
-                        Integer temperatureInt = Integer.parseInt(request.getParameter("temperature"));
-                        userPlant.setTemperatureNow(temperatureInt);
-                    } catch (NumberFormatException nfe) {
-                        upm.setErrors(true);
-                        upm.setStatus("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
-                        System.out.println("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
-                        nfe.printStackTrace();
-                    }
-                }
-
-                // soilmoisture
-                String soilmoisture = request.getParameter("soilmoisture");
-                if (soilmoisture != null) {
-                    try {
-                        Integer soilmoistureInt = Integer.parseInt(request.getParameter("soilmoisture"));
-                        userPlant.setSoilmoistureNow(soilmoistureInt);
-                    } catch (NumberFormatException nfe) {
-                        upm.setErrors(true);
-                        upm.setStatus("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
-                        System.out.println("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
-                        nfe.printStackTrace();
-                    }
-                }
-
-                upm.setUserPlant(userPlant);
-                upm.update(userPlant);
-            } else {
-                upm.setErrors(true);
-                upm.setStatus("Die Pflanze mit der ID '" + id + "' wurde nicht gefunden.");
-            }
             RequestDispatcher rd
-                    = request.getRequestDispatcher("/watering/Watering.jsp");
+                    = request.getRequestDispatcher("/arduino/Watering.jsp");
             rd.forward(request, response);
         }
     }
@@ -178,4 +88,105 @@ public class Watering extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    // <editor-fold defaultstate="collapsed" desc="wateringProcess method. Click on the + sign on the left to edit the code.">
+    private void wateringProcess(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        upm.setErrors(false);
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        UserPlant userPlant = upm.findUserPlantById(id);
+
+        if (userPlant != null) {
+
+            // watering
+            String watering = request.getParameter("watering");
+            if (watering != null) {
+                if (watering.equals("now")) {
+                    Date date = new Date();
+                    userPlant.setWateringDate(date);
+                }
+            }
+
+            // waterlevel
+            String waterlevel = request.getParameter("waterlevel");
+            if (waterlevel != null) {
+                try {
+                    Integer waterlevelInt = Integer.parseInt(request.getParameter("waterlevel"));
+                    userPlant.setWaterlevel(waterlevelInt);
+                } catch (NumberFormatException nfe) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
+                    System.out.println("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
+                    nfe.printStackTrace();
+                }
+
+            }
+
+            // humidity
+            String humidity = request.getParameter("humidity");
+            if (humidity != null) {
+                try {
+                    Integer humidityInt = Integer.parseInt(request.getParameter("humidity"));
+                    userPlant.setHumidityNow(humidityInt);
+                } catch (NumberFormatException nfe) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
+                    System.out.println("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
+                    nfe.printStackTrace();
+                }
+            }
+
+            // light
+            String light = request.getParameter("light");
+            if (light != null) {
+                try {
+                    Integer lightInt = Integer.parseInt(request.getParameter("light"));
+                    userPlant.setLightNow(lightInt);
+                } catch (NumberFormatException nfe) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
+                    System.out.println("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
+                    nfe.printStackTrace();
+                }
+            }
+
+            // temperature
+            String temperature = request.getParameter("temperature");
+            if (temperature != null) {
+                try {
+                    Integer temperatureInt = Integer.parseInt(request.getParameter("temperature"));
+                    userPlant.setTemperatureNow(temperatureInt);
+                } catch (NumberFormatException nfe) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
+                    System.out.println("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
+                    nfe.printStackTrace();
+                }
+            }
+
+            // soilmoisture
+            String soilmoisture = request.getParameter("soilmoisture");
+            if (soilmoisture != null) {
+                try {
+                    Integer soilmoistureInt = Integer.parseInt(request.getParameter("soilmoisture"));
+                    userPlant.setSoilmoistureNow(soilmoistureInt);
+                } catch (NumberFormatException nfe) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
+                    System.out.println("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
+                    nfe.printStackTrace();
+                }
+            }
+
+            upm.setUserPlant(userPlant);
+            upm.update(userPlant);
+        } else {
+            upm.setErrors(true);
+            upm.setStatus("Die Pflanze mit der ID '" + id + "' wurde nicht gefunden.");
+        }
+    }// </editor-fold>
+
+    private void calculateHealth(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
 }
