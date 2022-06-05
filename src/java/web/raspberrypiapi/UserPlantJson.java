99a9857f2853db85e3290008696faa773af63647
Christian <100922898+c-kierdorf@users.noreply.github.com>
2022-06-06 00:22:17 +0200

    Add field waterNow

--- src/java/web/raspberrypiapi/UserPlantJson.java
+++ src/java/web/raspberrypiapi/UserPlantJson.java
@@ -5,14 +5,19 @@ public class UserPlantJson {
     private Integer id;
     private String name;
     private Integer transferIntervall;
+    private boolean waterNow;
     
     public UserPlantJson() {
     }
 
-    public UserPlantJson(Integer id, String name, Integer transferIntervall) {
+    public UserPlantJson(Integer id, 
+                         String name, 
+                         Integer transferIntervall,
+                         boolean waterNow) {
         this.id = id;
         this.name = name;
         this.transferIntervall = transferIntervall;
+        this.waterNow = waterNow;
     }
 
     public Integer getId() {
@@ -38,4 +43,13 @@ public class UserPlantJson {
     public void setTransferIntervall(Integer transferIntervall) {
         this.transferIntervall = transferIntervall;
     }
+
+    public boolean isWaterNow() {
+        return waterNow;
+    }
+
+    public void setWaterNow(boolean waterNow) {
+        this.waterNow = waterNow;
+    }
+    
 }
