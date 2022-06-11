8481cbeba57d65641eddf22a7b3872502a911130
Christian <100922898+c-kierdorf@users.noreply.github.com>
2022-06-11 16:05:25 +0200

    Create JSON Objects the raspberry pi pico can read Delete all code relating to arduino

--- src/java/web/raspberrypiapi/Watering.java
+++ src/java/web/raspberrypiapi/Watering.java
@@ -1,11 +1,12 @@
 package web.raspberrypiapi;
 
+import com.google.gson.Gson;
 import db.UserPlant;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.List;
 import javax.inject.Inject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +23,7 @@ public class Watering extends HttpServlet {
 
     @Inject
     private UserPlantManager upm;
+    private Gson gson = new Gson();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,24 +33,49 @@ public class Watering extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs Allowed GET-Request-patterns:
-     * Der Arduino kann keine HTTPS Anfragen senden. Deshalb muss folgende URL
-     * mit HTTP statt HTTPS verwendet werden:
-     * http://gardenly.garden:3080/Watering?id=9&waterlevel=70&watering=now&humidity=40&light=50&temperature=12&soilmoisture=80
-     * Update 7.5.22: wahrscheinlich funktionieren einige Sensoren nicht. 
-     * Dann muss der Request so aussehen:
-     * http://gardenly.garden:3080/Watering?id=9&waterlevel=70&watering=now&soilmoisture=80
      *
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            UserPlantJson userPlantJson = new UserPlantJson();
+            String userPlantJsonString = "";
+            Date date = new Date();
 
-            wateringProcess(request, response);
+            List<UserPlant> userPlants = upm.findAll();
+            if (userPlants != null) {
+                userPlants.removeIf(i -> i.isIsConnected() != true);
 
-            RequestDispatcher rd
-                    = request.getRequestDispatcher("/arduino/Watering.jsp");
-            rd.forward(request, response);
+                try {
+
+                    UserPlant userPlantConnected = userPlants.get(0);
+                    Integer id = Integer.parseInt(request.getParameter("id"));
+                    UserPlant userPlant = upm.findUserPlantById(id);
+                    if (userPlantConnected.getPlantsFk().getPlantsId().equals(userPlant.getPlantsFk().getPlantsId())) {
+                        userPlant.setWaterNow(false);
+                        userPlant.setWateringDate(date);
+                        upm.update(userPlant);
+                        userPlantJson.setId(userPlant.getUserPlantsId());
+                        userPlantJson.setName(userPlant.getUserPlantName());
+                        userPlantJson.setTransferIntervall(userPlant.getTransferInterval());
+                        userPlantJson.setWaterNow(userPlant.isWaterNow());
+                        userPlantJsonString = gson.toJson(userPlantJson);
+                        out.print(userPlantJsonString);
+                        upm.setErrors(false);
+                    } else {
+                        upm.setErrors(true);
+                        upm.setStatus("Die UserPlant mit der ID " + id + " ist nicht mit der Hardware ID 1 verbunden.");
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    upm.setErrors(true);
+                    upm.setStatus("Es ist derzeit keine UserPlant mit der Hardware ID 1 verbunden.");
+                }
+            } else {
+                upm.setErrors(true);
+                upm.setStatus("Es wurden noch keine UserPlants angelegt.");
+            }
         }
     }
 
@@ -90,106 +117,4 @@ public class Watering extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    // <editor-fold defaultstate="collapsed" desc="wateringProcess method. Click on the + sign on the left to edit the code.">
-    private void wateringProcess(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        upm.setErrors(false);
-        Integer id = Integer.parseInt(request.getParameter("id"));
-        UserPlant userPlant = upm.findUserPlantById(id);
-
-        if (userPlant != null) {
-
-            // watering
-            String watering = request.getParameter("watering");
-            if (watering != null) {
-                if (watering.equals("now")) {
-                    Date date = new Date();
-                    userPlant.setWateringDate(date);
-                }
-            }
-
-            // waterlevel
-            String waterlevel = request.getParameter("waterlevel");
-            if (waterlevel != null) {
-                try {
-                    Integer waterlevelInt = Integer.parseInt(request.getParameter("waterlevel"));
-                    userPlant.setWaterlevel(waterlevelInt);
-                } catch (NumberFormatException nfe) {
-                    upm.setErrors(true);
-                    upm.setStatus("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
-                    System.out.println("Es wurde keine gültige Zahl für den Wasserstand eingegeben.");
-                    nfe.printStackTrace();
-                }
-
-            }
-
-//            // humidity
-//            String humidity = request.getParameter("humidity");
-//            if (humidity != null) {
-//                try {
-//                    Integer humidityInt = Integer.parseInt(request.getParameter("humidity"));
-//                    userPlant.setHumidityNow(humidityInt);
-//                } catch (NumberFormatException nfe) {
-//                    upm.setErrors(true);
-//                    upm.setStatus("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
-//                    System.out.println("Es wurde keine gültige Zahl für die Luftfeuchtigkeit eingegeben.");
-//                    nfe.printStackTrace();
-//                }
-//            }
-//
-//            // light
-//            String light = request.getParameter("light");
-//            if (light != null) {
-//                try {
-//                    Integer lightInt = Integer.parseInt(request.getParameter("light"));
-//                    userPlant.setLightNow(lightInt);
-//                } catch (NumberFormatException nfe) {
-//                    upm.setErrors(true);
-//                    upm.setStatus("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
-//                    System.out.println("Es wurde keine gültige Zahl für den Lichteinfluss eingegeben.");
-//                    nfe.printStackTrace();
-//                }
-//            }
-//
-//            // temperature
-//            String temperature = request.getParameter("temperature");
-//            if (temperature != null) {
-//                try {
-//                    Integer temperatureInt = Integer.parseInt(request.getParameter("temperature"));
-//                    userPlant.setTemperatureNow(temperatureInt);
-//                } catch (NumberFormatException nfe) {
-//                    upm.setErrors(true);
-//                    upm.setStatus("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
-//                    System.out.println("Es wurde keine gültige Zahl für die Temperatur eingegeben.");
-//                    nfe.printStackTrace();
-//                }
-//            }
-
-            // soilmoisture
-            String soilmoisture = request.getParameter("soilmoisture");
-            if (soilmoisture != null) {
-                try {
-                    Integer soilmoistureInt = Integer.parseInt(request.getParameter("soilmoisture"));
-                    userPlant.setSoilmoistureNow(soilmoistureInt);
-                } catch (NumberFormatException nfe) {
-                    upm.setErrors(true);
-                    upm.setStatus("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
-                    System.out.println("Es wurde keine gültige Zahl für die Bodenfeuchtigkeit eingegeben.");
-                    nfe.printStackTrace();
-                }
-            }
-
-            upm.setUserPlant(userPlant);
-            upm.update(userPlant);
-        } else {
-            upm.setErrors(true);
-            upm.setStatus("Die Pflanze mit der ID '" + id + "' wurde nicht gefunden.");
-        }
-    }// </editor-fold>
-
-    private void calculateHealth(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //TODO: calculate health
-    }
 }
