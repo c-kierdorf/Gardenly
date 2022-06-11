fa8841f28ebd88f2dbe82d9c753123672ab84cfe
Christian <100922898+c-kierdorf@users.noreply.github.com>
2022-06-11 16:06:23 +0200

    Build UserPlantWaterStopSuccess.java

---
+++ src/java/web/userplants/UserPlantWaterStopSuccess.java
@@ -0,0 +1,97 @@
+package web.userplants;
+
+import db.UserPlant;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.userplant.UserPlantManager;
+
+/**
+ *
+ * @author CK
+ */
+@WebServlet(name = "UserPlantWaterStopSuccess", urlPatterns = {"/UserPlantWaterStopSuccess"})
+public class UserPlantWaterStopSuccess extends HttpServlet {
+
+    @Inject
+    private UserPlantManager upm;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            Integer id = Integer.parseInt(request.getParameter("up_id"));
+            UserPlant userPlant = upm.findUserPlantById(id);
+
+            if (userPlant != null) {
+                userPlant.setWaterNow(false);
+                upm.setUserPlant(userPlant);
+                upm.update(userPlant);
+                upm.setErrors(false);
+            } else {
+                upm.setErrors(true);
+                upm.setStatus("Die UserPlant mit der ID " + id + " konnte nicht in der DB gefunden werden.");
+            }
+
+            RequestDispatcher rd
+                    = request.getRequestDispatcher("/user-plants/UserPlantWaterStopSuccess.jsp");
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
