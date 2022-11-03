95be7f8a820eeba28f126e9610fd1d7a3812bdef
Christian <100922898+c-kierdorf@users.noreply.github.com>
2022-11-03 21:49:41 +0100

    Add parameters to fetch new question items. Renamed item codes

--- src/java/web/questionaire/PostQuestionaireLanding.java
+++ src/java/web/questionaire/PostQuestionaireLanding.java
@@ -5,7 +5,6 @@ import db.Participant;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
-import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -42,122 +41,174 @@ public class PostQuestionaireLanding extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
 
-            String nickName = request.getParameter("nickName");
-            List<Participant> participants = pam.findParticipantByName(nickName);
-            if (!participants.isEmpty()) {
-                Participant participant = participants.get(0);
-                pam.setParticipant(participant);
+            Integer participantId = Integer.parseInt(request.getParameter("participantId"));
+            Participant participant = pam.findParticipantById(participantId);
+
+            pam.setParticipant(participant);
+
+            String pe1 = request.getParameter("pe1");
+            String pe2 = request.getParameter("pe2");
+            String pe3 = request.getParameter("pe3");
+            String pe4 = request.getParameter("pe4");
+            String ee1 = request.getParameter("ee1");
+            String ee2 = request.getParameter("ee2");
+            String ee3 = request.getParameter("ee3");
+            String ee4 = request.getParameter("ee4");
+            String si1 = request.getParameter("si1");
+            String si2 = request.getParameter("si2");
+            String si3 = request.getParameter("si3");
+            String fc1 = request.getParameter("fc1");
+            String fc2 = request.getParameter("fc2");
+            String fc3 = request.getParameter("fc3");
+            String fc4 = request.getParameter("fc4");
+            String hm1 = request.getParameter("hm1");
+            String hm2 = request.getParameter("hm2");
+            String hm3 = request.getParameter("hm3");
+            String pv1 = request.getParameter("pv1");
+            String pv2 = request.getParameter("pv2");
+            String pv3 = request.getParameter("pv3");
+            String ht1 = request.getParameter("ht1");
+            String ht2 = request.getParameter("ht2");
+            String ht3 = request.getParameter("ht3");
+            String ht4 = request.getParameter("ht4");
+            String bi1 = request.getParameter("bi1");
+            String bi2 = request.getParameter("bi2");
+            String bi3 = request.getParameter("bi3");
+            String use = request.getParameter("use");
+            String sq1 = request.getParameter("sq1");
+            String sq2 = request.getParameter("sq2");
+            String sq3 = request.getParameter("sq3");
+            String sq4 = request.getParameter("sq4");
+            String sq5 = request.getParameter("sq5");
+            String hq1 = request.getParameter("hq1");
+            String hq2 = request.getParameter("hq2");
+            String hq3 = request.getParameter("hq3");
+            String hq4 = request.getParameter("hq4");
+            String hq5 = request.getParameter("hq5");
+            String lob = request.getParameter("lob");
+            String kritik = request.getParameter("kritik");
+            String weiterentwicklung = request.getParameter("weiterentwicklung");
+
+            Date date = new Date();
+
+            PostQuestionaire postQuestionaire
+                    = new PostQuestionaire(participant,
+                                            pe1,
+                                            pe2,
+                                            pe3,
+                                            pe4,
+                                            ee1,
+                                            ee2,
+                                            ee3,
+                                            ee4,
+                                            si1,
+                                            si2,
+                                            si3,
+                                            fc1,
+                                            fc2,
+                                            fc3,
+                                            fc4,
+                                            hm1,
+                                            hm2,
+                                            hm3,
+                                            pv1,
+                                            pv2,
+                                            pv3,
+                                            ht1,
+                                            ht2,
+                                            ht3,
+                                            ht4,
+                                            bi1,
+                                            bi2,
+                                            bi3,
+                                            use,
+                                            sq1,
+                                            sq2,
+                                            sq3,
+                                            sq4,
+                                            sq5,
+                                            hq1,
+                                            hq2,
+                                            hq3,
+                                            hq4,
+                                            hq5,
+                                            lob,                
+                                            kritik,
+                                            weiterentwicklung,
+                                            date);
+
+            postqm.create(postQuestionaire);
+
+            SendPostQuestionaireEmail postEmail = new SendPostQuestionaireEmail();
+
+            boolean sendPostEmail = postEmail.sendEmail(participant,
+                                                        pe1,
+                                                        pe2,
+                                                        pe3,
+                                                        pe4,
+                                                        ee1,
+                                                        ee2,
+                                                        ee3,
+                                                        ee4,
+                                                        si1,
+                                                        si2,
+                                                        si3,
+                                                        fc1,
+                                                        fc2,
+                                                        fc3,
+                                                        fc4,
+                                                        hm1,
+                                                        hm2,
+                                                        hm3,
+                                                        pv1,
+                                                        pv2,
+                                                        pv3,
+                                                        ht1,
+                                                        ht2,
+                                                        ht3,
+                                                        ht4,
+                                                        bi1,
+                                                        bi2,
+                                                        bi3,
+                                                        use,
+                                                        sq1,
+                                                        sq2,
+                                                        sq3,
+                                                        sq4,
+                                                        sq5,
+                                                        hq1,
+                                                        hq2,
+                                                        hq3,
+                                                        hq4,
+                                                        hq5,
+                                                        lob,                
+                                                        kritik,
+                                                        weiterentwicklung);
+
+            if (sendPostEmail) {
                 postqm.setErrors(false);
-
-                String q1 = request.getParameter("q1");
-                String q2 = request.getParameter("q2");
-                String q3 = request.getParameter("q3");
-                String q4 = request.getParameter("q4");
-                String q5 = request.getParameter("q5");
-                String q6 = request.getParameter("q6");
-                String q7 = request.getParameter("q7");
-                String q8 = request.getParameter("q8");
-                String q9 = request.getParameter("q9");
-                String q10 = request.getParameter("q10");
-                String q11 = request.getParameter("q11");
-                String q12 = request.getParameter("q12");
-                String q13 = request.getParameter("q13");
-                String q14 = request.getParameter("q14");
-                String q15 = request.getParameter("q15");
-                String q16 = request.getParameter("q16");
-                String q17 = request.getParameter("q17");
-                String q18 = request.getParameter("q18");
-                String q19 = request.getParameter("q19");
-                String kritik = request.getParameter("kritik");
-                String lob = request.getParameter("lob");
-                String weiterentwicklung = request.getParameter("weiterentwicklung");
-                
-                Date date = new Date();
-                
-                PostQuestionaire postQuestionaire
-                        = new PostQuestionaire(participant, 
-                                                q1,
-                                                q2,
-                                                q3,
-                                                q4,
-                                                q5,
-                                                q6,
-                                                q7,
-                                                q8,
-                                                q9,
-                                                q10,
-                                                q11,
-                                                q12,
-                                                q13,
-                                                q14,
-                                                q15,
-                                                q16,
-                                                q17,
-                                                q18,
-                                                q19,
-                                                kritik,
-                                                lob,
-                                                weiterentwicklung,
-                                                date);
-
-                postqm.create(postQuestionaire);
-
-                SendPostQuestionaireEmail postEmail = new SendPostQuestionaireEmail();
-
-                boolean sendPostEmail = postEmail.sendEmail(nickName,
-                                                            participant.getEmail(),
-                                                            q1,
-                                                            q2,
-                                                            q3,
-                                                            q4,
-                                                            q5,
-                                                            q6,
-                                                            q7,
-                                                            q8,
-                                                            q9,
-                                                            q10,
-                                                            q11,
-                                                            q12,
-                                                            q13,
-                                                            q14,
-                                                            q15,
-                                                            q16,
-                                                            q17,
-                                                            q18,
-                                                            q19,
-                                                            kritik,
-                                                            lob,
-                                                            weiterentwicklung);
-
-                if (sendPostEmail) {
-                    postqm.setErrors(false);
-                } else {
-                    postqm.setErrors(true);
-                    postqm.setStatus("Daten gespeichert, aber Fehler beim Senden der E-Mail");
-                }
-
             } else {
                 postqm.setErrors(true);
-                postqm.setStatus("Keine Einträge zum Namen " + nickName + " gefunden.");
+                postqm.setStatus("Daten gespeichert, aber Fehler beim Senden der E-Mail");
             }
 
-            RequestDispatcher rd
-                    = request.getRequestDispatcher("PostQuestionaireLanding.jsp");
-            rd.forward(request, response);
-        }
+        RequestDispatcher rd
+                = request.getRequestDispatcher("PostQuestionaireLanding.jsp");
+        rd.forward(request, response);
     }
+}
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+/**
+ * Handles the HTTP <code>GET</code> method.
+ *
+ * @param request servlet request
+ * @param response servlet response
+ * @throws ServletException if a servlet-specific error occurs
+ * @throws IOException if an I/O error occurs
+ */
+@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -171,7 +222,7 @@ public class PostQuestionaireLanding extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -182,7 +233,7 @@ public class PostQuestionaireLanding extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo() {
+        public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
